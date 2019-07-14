c129d82a132b8e3640d978b8f6540dfc77a71857
Jmardura <jarduraterra@gmail.com>
2019-07-14 10:06:08 +0200

    afegits metodes per frenar i accelerar a propulsors i a coet.

--- src/Coet.java
+++ src/Coet.java
@@ -4,16 +4,33 @@ import java.util.Arrays;
 public class Coet {
 	private String name;
 	private ArrayList<Propulsor> propulsors;
-	
-	public Coet(String nom ,ArrayList<Propulsor> propulsors) {
-		this.name= nom;
+
+	public Coet(String nom, ArrayList<Propulsor> propulsors) {
+		this.name = nom;
 		this.propulsors = propulsors;
-		
+
+	}
+
+	@Override
+	public String toString() {
+		int[] valorsPropulsors = new int[propulsors.size()];
+		for (int i = 0; i < propulsors.size(); i++) {
+			valorsPropulsors[i] = (propulsors.get(i).potencia);
+		}
+		return ("El coet " + name + " té " + propulsors.size() + " propulsors." + Arrays.toString(valorsPropulsors));
 	}
 	
-	@Override
-	  public String toString() {
-	    return ("El coet " + name + " té " + propulsors.size() + " propulsors." + Arrays.toString(propulsors));
-	  }
+	public void acceleraCoet() {
+		for (int i = 0; i < propulsors.size(); i++) {
+			propulsors.get(i).accelProp(10);
+		}
+	}
+	
+	public void frenaCoet() {
+		for (int i = 0; i < propulsors.size(); i++) {
+			propulsors.get(i).frenaProp(10);
+		}
+	}
+
 
 }
--- src/Propulsor.java
+++ src/Propulsor.java
@@ -1,9 +1,32 @@
 
 public class Propulsor {
 	int potencia;
+	int potenciaActual;
 	
-	public Propulsor(int potencia) {
+	public Propulsor(int potencia, int maxPotencia) {
 		this.potencia = potencia;
+		this.potenciaActual = 0;
+		
+	}
+	@Override
+	  public String toString() {
+		return ("la potencia maxima d'aquest propulsor es de " + potencia + " i actualment esta a una potencia de " + potenciaActual);
 	}
 	
+	public int accelProp(int a) {
+		if((potenciaActual + a)<=potencia) {
+			return (potenciaActual+a);
+		}else {
+			potenciaActual= potencia;
+			return potencia;
+		}
+	}
+	public int frenaProp(int a) {
+		if((potenciaActual - a >=0)) {
+			return (potenciaActual-a);
+		}else {
+			potenciaActual=0;
+			return potenciaActual;
+		}
+	}
 }
